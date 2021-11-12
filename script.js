c319c95d9df41ef92d486f77f9bfe25df3c66996
matteo9723 <matteolandri7@gmail.com>
2021-11-12 16:46:35 +0100

    aiuto

--- script.js
+++ script.js
@@ -3,6 +3,38 @@ const app = new Vue ({
   el:'#app',
 
   data:{
-    saluto:'ciao',
+    lista:['fare la spesa','fare la doccia','fare il letto'],
+    Input:'',
+    verifica:false,
+    fatto:'check',
+    disattivi:0,
+  },
+
+    
+  
+
+  mounted(){},
+
+  methods:{
+    remove(index){
+      this.lista.splice(index,1);
+    },
+
+    insertValue(){
+
+      if(this.Input.length > 3){
+        this.lista.push(this.Input);
+        this.Input='';
+      }else{
+        this.verifica=true;
+        setTimeout(()=>{
+          this.verifica=false
+        },2000)
+      }
+      
+    },
+
+    
   }
-})
+
+});
